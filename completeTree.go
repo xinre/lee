e10223061b487678f65dbe7c01ad763e90ade9c0
tanghongyu <505648237@qq.com>
2020-10-12 00:28:35 +0800

    build complete tree

--- completeTree.go
+++ completeTree.go
@@ -7,7 +7,6 @@ type ItemTree interface {
 
 type TreeNode struct {
 	value ItemTree
-	Parent *TreeNode
 	left  *TreeNode
 	right *TreeNode
 }
@@ -17,5 +16,45 @@ type CompleteTree struct {
 }
 
 func (rootTree *CompleteTree) BuildTree(arr int[]) {
+	len := len(arr)
+	saveArr := []TreeNode{}
+	index := 0
 	
+	for i := 0; i < len; i++ {
+		node := &TreeNode{ arr[i], nil, nil }
+		saveArr.append(node)
+	}
+
+	rootTree.Root = saveArr[0]
+
+	for index < len {
+		leftIndex := 2 * index + 1
+		rightIndex := 2 * index + 2
+		saveArr[index].left = saveArr[leftIndex];
+		saveArr[index].right = saveArr[rightIndex]
+	}
+}
+
+func (rootTree *CompleteTree) RecurrenceBuildTree(arr int[]) {
+	rootTree.Root = &TreeNode{ arr[i], nil, nil }
+	len := len(arr)
+
+	func recurrenceFun (nodes []TreeNode{}, i int) {
+		leftIndex := 2 * i + 1
+		rightIndex := 2 * i + 2
+
+		if leftIndex < len {
+			node := &TreeNode{ arr[i], nil, nil }
+			nodes.left = node
+			recurrenceFun(node, leftIndex)
+		}
+
+		if rightIndex < len {
+			node := &TreeNode{ arr[i], nil, nil }
+			nodes.right = node
+			recurrenceFun(node, rightIndex)
+		}
+	}
+
+	recurrenceFun(rootTree.Root, 0)
 }
